d78d6428183cd62452fd1488cc1c12e4a1b203d8
dev-manindepth <manindepth07@gmail.com>
2023-09-20 08:40:05 +0530

    feat: Implement user signout functionality

--- src/features/auth/routes/auth.routes.ts
+++ src/features/auth/routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import { Signin } from '@auth/controllers/signin';
+import { Signout } from '@auth/controllers/signout';
 import { Signup } from '@auth/controllers/signup';
 import { Router } from 'express';
 import express from 'express';
@@ -11,6 +12,10 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', Signup.prototype.create);
     this.router.post('/signin', Signin.prototype.read);
+    return this.router;
+  }
+  public signoutRoute(): Router {
+    this.router.get('/signout', Signout.prototype.update);
     return this.router;
   }
 }
